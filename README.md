d4e4ae086e5995968d3848b6ddf0ab45f050c39d
Kirill Maslov <maslovks@gmail.com>
2018-01-09 22:01:11 +0300

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Github Trends
+The script shows the list of 20 the most popular github projects, created during last 7 days.
 
-[TODO. There will be project description]
+To run the script one has to use python3.5. All side packages are listed in requirements.txt. To install them from the file one can run the following command in the shell:
+```
+pip install -r requirements.txt
+```
+
+# How to use
+```
+python github_trending.py
+ 1. repo-owner: paboldin
+	repo-name: meltdown-exploit
+	578 stars
+	10 issues
+	https://github.com/paboldin/meltdown-exploit
+ 2. repo-owner: raphaelsc
+	repo-name: Am-I-affected-by-Meltdown
+	447 stars
+	6 issues
+	https://github.com/raphaelsc/Am-I-affected-by-Meltdown
+    ...
+```
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,42 @@
+import requests
+import datetime
 
 
 def get_trending_repositories(top_size):
-    pass
+    url = 'https://api.github.com/search/repositories'
+    date_boundary = datetime.date.today() - datetime.timedelta(days=7)
+
+    params = {
+        'q': 'created:>{}'.format(date_boundary.isoformat()),
+        'sort': 'stars',
+        'order': 'desc',
+        'per_page': top_size,
+    }
+    response = requests.get(url, params=params)
+    return response.json()['items']
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    url = 'https://api.github.com/repos/{}/{}/issues'.format(
+        repo_owner,
+        repo_name,
+    )
+    params = {'state': 'open'}
+    response = requests.get(url, params=params)
+    return len(response.json())
+
 
 if __name__ == '__main__':
-    pass
+    trending_repos = get_trending_repositories(20)
+    for rank, repo in enumerate(trending_repos, 1):
+        repo_name = repo['name']
+        repo_owner = repo['owner']['login']
+        stars_number = repo['stargazers_count']
+        repo_url = repo['html_url']
+
+        amount_of_issues = get_open_issues_amount(repo_owner, repo_name)
+        print('{:2}. repo-owner: {}'.format(rank, repo_owner))
+        print('\trepo-name: {}'.format(repo_name))
+        print('\t{} stars'.format(stars_number))
+        print('\t{} issues'.format(amount_of_issues))
+        print('\t{}'.format(repo_url))
